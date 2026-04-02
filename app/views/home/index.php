524381e7c461e1410d6ffaa70c3a36665b19e907
JacoboBT <233518505+jacobobt@users.noreply.github.com>
2026-04-02 15:38:15 +0200

    Probar lectura de datos desde MySQL con PDO

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,2 +1,10 @@
 <h1><?php echo APP_NAME; ?></h1>
 <p>Proyecto base en PHP sin framework</p>
+
+<?php
+$sql = "SELECT COUNT(*) AS total FROM especialidades";
+$stmt = $pdo->query($sql);
+$resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<p>Total de especialidades en la base de datos: <?php echo $resultado['total']; ?></p>
