16d35bc17ef41f11b9c4c9bc0850de35bacbda64
Marcpr99 <marcpr992002@gmail.com>
2026-04-12 16:09:59 +0200

    Añadir campo precio.

--- sql/bbddReparaYa.sql
+++ sql/bbddReparaYa.sql
@@ -31,6 +31,7 @@ CREATE TABLE incidencias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     localizador VARCHAR(20) NOT NULL UNIQUE, -- Código tipo REP-2026-XXXX
     telefono_contacto VARCHAR(20) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL DEFAULT 0.00, -- Precio del servicio
     franja_horaria ENUM('09:00-13:00','16:00-20:00') NOT NULL DEFAULT '09:00-13:00',
     cliente_id INT NOT NULL,
     tecnico_id INT DEFAULT NULL, -- El admin lo asignará después
